409f301eb4e3f744f3a59b66505270c7d6521136
siavashg <siavash@tictail.com>
2014-03-03 19:57:56 -0500

    Allow disabling debug

--- debug.h
+++ debug.h
@@ -2,9 +2,13 @@
 #define DEBUG_H
 
 #define DEBUG 1
+#ifdef DEBUG
 #define print_debug(...) \
     do { if (DEBUG) fprintf(stderr, "%s:%d:%s(): ", __FILE__, __LINE__, \
                             __func__); \
                     fprintf(stderr, __VA_ARGS__); } while(0)
+#else
+#define print_debug(...) do { } while(0)
+#endif
 
 #endif // DEBUG_H
