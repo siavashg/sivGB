d7d4198b8fc84f3b95f6f7551149a84eaa7c6732
Siavash Ghorbani <siavashg@gmail.com>
2019-07-23 23:03:57 +0200

    Fix invalid init value for register C

--- z80.c
+++ z80.c
@@ -19,7 +19,7 @@ int reset_z80(Z80 *z80) {
     z80->a = 0x01;
     z80->f = 0xB0;
     z80->b = 0x00;
-    z80->c = 0x13;
+    z80->c = 0x0D;
     z80->d = 0x00;
     z80->e = 0xD8;
     z80->h = 0x01;
@@ -34,30 +34,32 @@ int reset_z80(Z80 *z80) {
 
 void debug_dump(Z80 *z80) {
 #ifdef DEBUG_VERBOSE
-    print_debug(
-        "PC: 0x%.4x\t"
-        "SP: 0x%.4x\t"
+    if (DEBUG_VERBOSE) {
+        print_debug(
+            "\nPC: 0x%.4X\t"
+            "SP: 0x%.4X\t\n"
 
-        "A: 0x%.2x\t"
-        "B: 0x%.2x\t"
-        "C: 0x%.2x\t"
-        "D: 0x%.2x\t"
+            "A: 0x%.2X\t"
+            "B: 0x%.2X\t"
+            "C: 0x%.2X\t"
+            "D: 0x%.2X\t\n"
 
-        "E: 0x%.2x\t"
-        "H: 0x%.2x\t"
-        "L: 0x%.2x\t"
-        "F: 0x%.2x\t"
+            "E: 0x%.2X\t"
+            "F: 0x%.2X\t"
+            "H: 0x%.2X\t"
+            "L: 0x%.2X\t\n"
 
-        "IME: 0x%.2x\t"
-        "M: 0x%.2x\t"
-        "T: 0x%.2x\t"
+            "IME: 0x%.2X\t"
+            "M: 0x%.2X\t"
+            "T: 0x%.2X\t"
 
-        "HALT: 0x%.2x\t"
-        "STOP: 0x%.2x\n",
-        z80->pc, z80->sp,
-        z80->a, z80->b, z80->c, z80->d,
-        z80->e, z80->h, z80->l, z80->f,
-        z80->ime, z80->m, z80->t,
-        z80->halt, z80->stop);
+            "HALT: 0x%.2X\t"
+            "STOP: 0x%.2X\n",
+            z80->pc, z80->sp,
+            z80->a, z80->b, z80->c, z80->d,
+            z80->e, z80->f, z80->h, z80->l,
+            z80->ime, z80->m, z80->t,
+            z80->halt, z80->stop);
+    }
 #endif
 }
